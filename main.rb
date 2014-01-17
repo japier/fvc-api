c2fe337fc90d682747a585a1e58218430b42521a
Javeir Arantes Do Nacimiento <javeir@Orange-PC.(none)>
2014-01-17 01:21:09 -0600

    New model and crose domain fix

--- main.rb
+++ main.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 require 'data_mapper'
+require 'json'
 require './model/models'
 
-get '/' do 
-	@categories = Cagegory.all
+before do
+  headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+  headers['Access-Control-Allow-Origin'] = 'http://localhost'
+  headers['Access-Control-Allow-Headers'] = 'accept, authorization, origin'
+  headers['Access-Control-Allow-Credentials'] = 'true'
+end
+get '/clientes' do 
+	@categories = Cliente.all
+	@categories.to_json
+end
+get '/clientes/:id_category' do
+	@category = Cliente.first(:id => params[:id_category])
+	@category.to_json
 end
--- model/models.rb
+++ model/models.rb
@@ -1,21 +1,32 @@
 require 'data_mapper'
 
-DataMapper.setup(:default, "mysql://root:monitores@localhost/vfc")
+DataMapper.setup(:default, "mysql://root:monitores@localhost/Reserva")
 
-class Category
+class Cliente
 	include DataMapper::Resource
 	property :id,			Serial
-	property :name, 		String, :required => true
-	property :complete_at, 	DateTime
-	property :hash_category, String, :required => true
+	property :nombre,		String
+	property :s_nombre,		String
+	property :ape, 			String
+	property :s_ape,		String
+	property :email,		String
+	property :tel, 			Decimal
+	property :fena,			DateTime
 end
+# class Category
+# 	include DataMapper::Resource
+# 	property :id,			Serial
+# 	property :name, 		String, :required => true
+# 	property :complete_at, 	DateTime
+# 	property :hash_category, String, :required => true, :key => true
+# end
 
-class Photo
-	include DataMapper::Resource
-	property :id, 			Serial
-	property :name, 		String, :required => true
-	property :uri, 			String, :required => true
-	property :fk_category,	Integer, :required => true
-	property :created_at,	DateTime
-end
+# class Photo
+# 	include DataMapper::Resource
+# 	property :id, 			Serial
+# 	property :name, 		String, :required => true
+# 	property :uri, 			String, :required => true
+# 	property :fk_category,	Integer, :required => true
+# 	property :created_at,	DateTime
+# end
 DataMapper.finalize
